80040fb116ff83ffbf08e6836667fb635928c7a4
KRS <131356071+Krs-Scripts@users.noreply.github.com>
2024-01-22 00:37:37 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -39,45 +39,33 @@ exports.ox_target:addModel(Config.AtmProp, {
 -- ██ ▄▄▀█ ▀▀ ██ █ █ ██ ▄▀█
 -- ██ ▀▀ █ ██ ██ ██▄ ██ ██ 
 -- ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-local points = {}
-
 CreateThread(function()
-
     for i = 1, #Config.PosizioneBanche, 1 do
         local cfg = Config.PosizioneBanche[i]
 
         if cfg.blip.attiva then 
-			local blip = AddBlipForCoord(cfg.posizione.x, cfg.posizione.y, cfg.posizione.z)
-			SetBlipSprite (blip, cfg.blip.sprite)
-			SetBlipDisplay(blip, 4)
-			SetBlipScale  (blip, cfg.blip.grandezza)
-			SetBlipColour (blip, cfg.blip.colore)
-			SetBlipAsShortRange(blip, true)
-			BeginTextCommandSetBlipName("STRING")
-			AddTextComponentString(cfg.blip.nome)
-			EndTextCommandSetBlipName(blip)
+
+            local blip = AddBlipForCoord(cfg.posizione.x, cfg.posizione.y, cfg.posizione.z)
+            SetBlipSprite(blip, cfg.blip.sprite)
+            SetBlipDisplay(blip, 4)
+            SetBlipScale(blip, cfg.blip.grandezza)
+            SetBlipColour(blip, cfg.blip.colore)
+            SetBlipAsShortRange(blip, true)
+            BeginTextCommandSetBlipName("STRING")
+            AddTextComponentString(cfg.blip.nome)
+            EndTextCommandSetBlipName(blip)
         end
 
         local modello = lib.requestModel(cfg.modello)
 
-        points[i] = lib.points.new({
-            coords = vector3(cfg.posizione.x, cfg.posizione.y, cfg.posizione.z),
-            onEnter = function()
-                npc = CreatePed(0, modello, cfg.posizione, false, true)
+        npc = CreatePed(0, modello, cfg.posizione, false, true)
 
-                SetModelAsNoLongerNeeded(modello)
-                FreezeEntityPosition(npc, true)
-                SetEntityInvincible(npc, true)
-                SetBlockingOfNonTemporaryEvents(npc, true)
-        
-                exports.ox_target:addLocalEntity(npc, Config.OpzioniTarget)
-            end,
-            onExit = function()
-                exports.ox_target:removeLocalEntity(npc)
-                DeleteEntity(npc)
-            end,
-            distance = 60,
-        })
+        SetModelAsNoLongerNeeded(modello)
+        FreezeEntityPosition(npc, true)
+        SetEntityInvincible(npc, true)
+        SetBlockingOfNonTemporaryEvents(npc, true)
+
+        exports.ox_target:addLocalEntity(npc, Config.OpzioniTarget)
     end
 end)
 -- ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
